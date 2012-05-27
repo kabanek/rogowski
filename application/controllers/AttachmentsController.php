b98b40b2b464f0366d4bce9c2bd59946ee085faa
Bartłomiej Kiełbasa <kontakt@bkielbasa.pl>
2012-05-27 17:32:23 +0200

    załatanie wygląda w ten sposób, że dzielę ścieżkę pliku wg separatora katalogów jakim jest w linuksie / a w windowsie \ i pobieram tylko ostatni człon czyli dla pliku ../public/index.php wynikiem będzie index.php

--- application/controllers/AttachmentsController.php
+++ application/controllers/AttachmentsController.php
@@ -8,6 +8,9 @@ class AttachmentsController extends BaseController
     {
         $file = base64_decode($_GET['file']);
 
+        $file = explode(PATH_SEPARATOR, $file);
+        $file = $file[count($file) - 1];
+
         $file_path =  '../attachments/' .$file;
 
         $mime_type = mime_content_type($file_path);
